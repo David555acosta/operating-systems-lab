a3eac6d1944dfd6422af6b265167f3ca26c72146
David Acosta <davidisrraelacostaz.9@gmail.com>
2026-09-22 17:41:18 -0300

    se agrego codigo c# de comunicacion de proceso emisor y receptor mediante cola de mensajeria

---
+++ comunicacion-procesos/Emisor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Messaging;
+class Program
+{
+    static void Main()
+    {
+        // Ruta de la cola privada local
+        string rutaCola = @".\Private$\ColaIPC";
+        // Crear la cola si no existe
+        if (!MessageQueue.Exists(rutaCola))
+        {
+            MessageQueue.Create(rutaCola);
+            Console.WriteLine("Cola creada con éxito.");
+        }
+        // Conectar a la cola y enviar el mensaje
+        using (MessageQueue cola = new MessageQueue(rutaCola))
+        {
+            string mensajeTexto = "Hola desde el proceso Emisor! - " +
+            DateTime.Now.ToString();
+            // Enviamos el objeto. MSMQ lo serializará automáticamente.
+            cola.Send(mensajeTexto, "EtiquetaMensaje");
+            Console.WriteLine($"Mensaje enviado: '{mensajeTexto}'");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ comunicacion-procesos/Receptor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Messaging;
+
+class Receptor
+{
+    static void Main()
+    {
+        string rutaCola = @".\Private$\ColaIPC";
+
+        if (!MessageQueue.Exists(rutaCola))
+        {
+            Console.WriteLine("La cola aún no existe. Ejecuta el Emisor primero.");
+            return;
+        }
+
+        using (MessageQueue cola = new MessageQueue(rutaCola))
+        {
+            // IMPORTANTE: Definir el formateador para decirle a MSMQ cómo
+            // deserializar el contenido
+            cola.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+            Console.WriteLine("Esperando mensajes en la cola...");
+
+            // El método Receive() bloquea el proceso hasta que llegue un mensaje
+            Message mensaje = cola.Receive();
+
+
+            string contenido = (string)mensaje.Body;
+            Console.WriteLine($"Mensaje recibido: '{contenido}'");
+        }
+    }
+}
